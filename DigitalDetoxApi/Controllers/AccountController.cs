1552bae74b7354660d7ad17d6f5056226c48bc50
Artyom <picheneg.epta@gmail.com>
2023-07-01 22:36:47 +0300

    add-packages

---
+++ DigitalDetoxApi/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalDetoxApi.Controllers;
+
+public class AccountController : BaseApiController
+{
+    [HttpPost("Register"), AllowAnonymous]
+    public async Task<ActionResult> Register()
+    {
+        // var user = new AppUser
+        // {
+        //     Name = registerDto.Email,
+        //     Email = registerDto.Email,
+        //     UserName = registerDto.UserName,
+        //     EmailConfirmed = false,
+        // };
+        //
+        // var usersWithSameEmail = await _userManager.FindByEmailsAsync(user.Email);
+        //
+        // if (usersWithSameEmail.Any(appUser => appUser.EmailConfirmed))
+        // {
+        //     return BadRequest("Email already taken");
+        // }
+        //
+        // var result = await _userManager.CreateAsync(user, registerDto.Password);
+        //
+        // foreach (var identityError in result.Errors)
+        // {
+        //     Console.WriteLine(identityError);
+        //
+        // }
+
+        return BadRequest();
+
+    }
+}
---
+++ DigitalDetoxApi/Controllers/BaseApiController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("API/[controller]")]
+public class BaseApiController : ControllerBase
+{
+        
+}
